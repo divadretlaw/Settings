e3ec407103e32b2e188ceda9e8a5e2d03e9035ad
David Walter <hello@davidwalter.at>
2021-02-06 15:27:04 +0100

    Update CheckMarkToggleStyle

--- Sources/Settings/Helper/CheckmarkToggleStyle.swift
+++ Sources/Settings/Helper/CheckmarkToggleStyle.swift
@@ -21,12 +21,37 @@ struct CheckmarkToggleStyle: ToggleStyle {
                     configuration.label
                         .foregroundColor(Color(.label))
                     Spacer()
-                    Image(systemName: "checkmark")
-                        .opacity(configuration.isOn ? 1 : 0)
-                        .accessibility(removeTraits: .isImage)
-                        .accessibility(addTraits: .isButton)
+                    if configuration.isOn {
+                        Image(systemName: "checkmark.circle.fill")
+                            .font(SwiftUI.Font.headline.weight(.bold))
+                            .accessibility(removeTraits: .isImage)
+                            .accessibility(addTraits: .isButton)
+                    } else {
+                        Image(systemName: "circle")
+                            .font(SwiftUI.Font.headline.weight(.light))
+                            .foregroundColor(Color(.systemGray2))
+                            .accessibility(removeTraits: .isImage)
+                            .accessibility(addTraits: .isButton)
+                    }
                 }
             })
         }
     }
 }
+
+#if DEBUG
+struct CheckmarkToggleStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        List {
+            Toggle(isOn: .constant(true), label: {
+                Text("True")
+            })
+            .toggleStyle(CheckmarkToggleStyle())
+            Toggle(isOn: .constant(false), label: {
+                Text("False")
+            })
+            .toggleStyle(CheckmarkToggleStyle())
+        }
+    }
+}
+#endif
